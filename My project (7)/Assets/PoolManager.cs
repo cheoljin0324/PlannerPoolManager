4f74bfaa000836d79a3ee90ae8b638c067bae0f0
cheoljin0324 <77821929+cheoljin0324@users.noreply.github.com>
2022-10-31 13:44:33 +0900

    Queue를 이용한 풀링

--- My project (7)/Assets/PoolManager.cs
+++ My project (7)/Assets/PoolManager.cs
@@ -12,32 +12,39 @@ public class PoolManager : MonoBehaviour
 
     private void Awake()
     {
-        for (int i = 0; i < newProfile.Length; i++)
-        {
-            GameObject newContainer = Instantiate(containerPrefab);
-            newContainer.name = newProfile[i].name;
-            for (int j = 0; j < newProfile[i].defalutAmount; j++)
-            {
-                GameObject prefab = Instantiate(newProfile[i].prefabSet, newContainer.transform);
-                prefab.transform.position = newProfile[i].defalutVector;
-            }
-        }
-
-
-
+        Init();
     }
 
 
-    private void Init()
+    public void Init()
     {
         for (int i = 0; i < newProfile.Length; i++)
         {
             GameObject newContainer = Instantiate(containerPrefab);
             newContainer.name = newProfile[i].name;
+            newProfile[i].container = newContainer;
             for (int j = 0; j < newProfile[i].defalutAmount; j++)
             {
                 GameObject prefab = Instantiate(newProfile[i].prefabSet, newContainer.transform);
                 prefab.transform.position = newProfile[i].defalutVector;
+                prefab.transform.parent = newContainer.transform;
+                newProfile[i].inConObject.Enqueue(prefab);
+            }
+        }
+    }
+
+    public static void PullRequest(GameObject pullObject, Vector3 pos, Vector3 rotation)
+    {
+
+    }
+
+    public static void PullRequest(string objName)
+    {
+        for(int i = 0; i<newProfile.Length; i++)
+        {
+            if(newProfile[i].name == objName)
+            {
+                
             }
         }
     }
@@ -48,7 +55,7 @@ public class PoolManager : MonoBehaviour
 [System.Serializable]
 struct PullProfile
 {
-    [Header("오브젝트 이름")]
+    [Header("컨테이너 이름")]
     public string name;
     [Header("기본 값 갯수")]
     public int defalutAmount;
@@ -56,6 +63,11 @@ struct PullProfile
     public int AddAmount;
     [Header("게임 오브젝트")]
     public GameObject prefabSet;
+    [Header("컨테이너")]
+    [HideInInspector]
+    public GameObject container;
     [Header("기본 위치 좌표")]
     public Vector3 defalutVector;
+    [Header("컨테이너 내부 오브젝트")]
+    public Queue<GameObject> inConObject;
 }
